0a84cb477adf9351bafb7bf1ce04d7b86c6461cf
rayc2045 <>
2022-01-18 23:25:23 +0800

    Complete project

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -1,7 +1,70 @@
-import { createApp } from '/src/assets/scripts/petite-vue.js';
+import { createApp } from './petite-vue.js';
 
 createApp({
+  isLoading: true,
+  colors: [
+    'red', 'red',
+    'yellow', 'yellow',
+    'blue', 'blue',
+    'gray',
+    'white', 'white', 'white', 'white', 'white', 'white',
+    'white', 'white', 'white', 'white', 'white', 'white',
+  ],
+  blocks: [],
+  id: 1,
+  generateNum: 12,
   init() {
-    
+    this.generateBlocks(this.generateNum);
+    window.onscroll = () => this.addBlocksAtTheBottom();
+    this.isLoading = false;
+  },
+  getRandomNum(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
+  getGridStyle(columnNum, rowNum) {
+    let [column, row] = ['', ''];
+    for (let i = 0; i < columnNum; i++) column += `${this.getRandomNum(1, 4)}fr `;
+    for (let i = 0; i < rowNum; i++) row += `${this.getRandomNum(1, 4)}fr `;
+    return `grid-template-columns: ${column.trim()}; grid-template-rows: ${row.trim()}`
+  },
+  getBackgroundColorClassNames(num) {
+    const classNames = [];
+    for (let i = 0; i < num; i++) {
+      classNames.push(
+        this.colors[this.getRandomNum(0, this.colors.length - 1)]
+      );
+    }
+    return classNames;
+  },
+  isTwoObjectsTheSame(a, b) {
+    return JSON.stringify(a) === JSON.stringify(b);
+  },
+  generateBlocks(n) {
+    let times = 0;
+    while (times < n) {
+      const [columnNum, rowNum] = [this.getRandomNum(2, 4), this.getRandomNum(2, 4)];
+      const block = {
+        id: this.id,
+        style: this.getGridStyle(columnNum, rowNum),
+        innerBlockClassNames: this.getBackgroundColorClassNames(columnNum * rowNum)
+      };
+      for (let i = 0; i < this.blocks.length; i++) {
+        if (this.isTwoObjectsTheSame(this.blocks[i], block)) continue;
+      }
+      this.blocks.push(block);
+      this.id++;
+      times++;
+    }
+  },
+  isAtTheBottom() {
+    const scrollTop = document.documentElement.scrollTop;
+    const clientHeight = document.documentElement.clientHeight;
+    const scrollHeight = document.documentElement.scrollHeight;
+    return scrollTop + clientHeight >= scrollHeight;
+  },
+  addBlocksAtTheBottom() {
+    if (this.isAtTheBottom()) this.generateBlocks(this.generateNum);
   }
 }).mount();
